8cb20a79986461eb0a8a026b32a6bd38f902f039
Jose Hernandez <josemanuelhdez2@gmail.com>
2016-01-12 00:06:19 +0000

    Progressingando

--- src/tsp/Solver.java
+++ src/tsp/Solver.java
@@ -9,6 +9,8 @@ import tsp.typedef.NodeArray;
  */
 public class Solver extends SolverHelper {
     
+    final Integer FST_NODE = 0; // Arbitrary first node for solutions.
+    
     Solver(DistanceMatrix dm){
         super(dm);
     }
@@ -18,12 +20,18 @@ public class Solver extends SolverHelper {
      */
     NodeArray nearestNeighbor(){
         NodeArray ift = new NodeArray(); // Initial feasible tour
+        ift.addNode(FST_NODE, 0d);
         
         while(!hasAllNodes(ift)){
-            
+            addNodeClosestToLast(ift);
         }
+        ift.addNode(FST_NODE, dm.get(ift.getLastNode(), FST_NODE)); // go back to start
         
         return ift;
     }
+    
+    /**
+     * Step 2: Prim heuristic for getting low bounds.
+     */
 
 }
--- src/tsp/SolverHelper.java
+++ src/tsp/SolverHelper.java
@@ -14,11 +14,39 @@ public class SolverHelper {
         this.dm = dm;
     }
     
-    protected addNode(NodeArray na, Integer node){
-        na.addNode(node, dm.get(na.getLastNode(),0));
-        return na;
+    // Used when adding closest unvisited node.
+    protected NodeArray unvisitedNodes(NodeArray na){
+        // Get all nodes in graph.
+        NodeArray allNodes = new NodeArray();
+        for(int i = 0; i < dm.width(); i++) allNodes.add(i);
+        
+        // Discard those not in tour.
+        NodeArray unvisited = new NodeArray(allNodes);
+        for (int i = 0; i < na.size(); i++) unvisited.remove(i);
+        
+        return unvisited;
     }
     
+    protected Integer findClosestUnvisited(NodeArray na, Integer node){
+        NodeArray unvisited = unvisitedNodes(na);
+        Integer closestUnvisited = null;
+        Double closestUnvisitedDist = Double.MAX_VALUE;
+        for (Integer un : unvisited){
+            if (dm.get(node, un) < closestUnvisitedDist){
+                closestUnvisited = un;
+                closestUnvisitedDist = dm.get(node, un);
+            }
+        }
+        return closestUnvisited;
+    }
+    
+    // Used in NN construction algorithm
+    protected void addNodeClosestToLast(NodeArray na){
+        Integer cu = findClosestUnvisited(na, na.getLastNode());
+        na.addNode(cu, dm.get(na.getLastNode(), cu));
+    }
+    
+    // Used for knowing when to stop.
     protected boolean hasAllNodes(NodeArray na){
         for (int i = 0; i < dm.width(); i++){
             if (!na.contains(i)) return false;
@@ -26,4 +54,17 @@ public class SolverHelper {
         return true;
     }
     
+    // Used in Prim heuristic
+    protected void addNodeClosestToAny(NodeArray na){
+        Integer cuOverall = null;   // "Closest unvisited overall" overall = to any node.
+        Double cuOverallDist = Double.MAX_VALUE;
+        for (int i = 0; i < dm.width(); i++){
+            Integer cuToI = findClosestUnvisited(na, i); // "Closest unvisited to i", i is current node.
+            if (dm.get(i, cuToI) < cuOverallDist){
+                cuOverall = findClosestUnvisited(na, i);
+                cuOverallDist = dm.get(i, cuToI);
+            }
+        }
+        na.addNode(cuOverall, dm.get(na.getLastNode(), cuOverall));
+    }
 }
